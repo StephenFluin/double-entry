3b044053ad5baa7f108860fddd87932e4647071b
Stephen Fluin <stephen.fluin@gmail.com>
2018-10-26 00:33:19 -0700

    add initial system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CentsPipe } from './cents.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CentsPipe,
   ],
   imports: [
     BrowserModule
